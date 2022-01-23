c350d36bcb406fcd6e05c024cec36431a909cadb
omcewan <orlandomcewan231@gmail.com>
2022-01-22 23:01:06 -0500

    weather display icon incorporated and weather functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ function getLatLon(searcCity) {
   var latLonApiUrl = `http://api.positionstack.com/v1/forward?access_key=0384ffba1373feef9d41f185c9d4d012&query=${searchCity}&country=US`;
   fetch(latLonApiUrl).then((response) => {
     response.json().then((data) => {
+        console.log(data)
       latitude = data.data[0].latitude;
       longitude = data.data[0].longitude;
       getWeatherInfo(latitude, longitude);
@@ -27,8 +28,9 @@ function getWeatherInfo(latitude, longitude) {
     "&exclude=minutely,hourly,alerts&units=imperial&appid=f4d4536da514f2ce7b14e71927f09061";
   fetch(cityWeatherApi).then((response) => {
     response.json().then((data) => {
-    //   console.log(data);
-      currentCityEl.innerHTML = "<h2>" + searchCity + " " + currentDate;
+      console.log(data);
+      currentCityEl.innerHTML = "<h2>" + searchCity + " " + currentDate + "<img src=http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png>" 
+      currentCityEl.innerHTML += "<p> Description: " + data.current.weather[0].description
       currentCityEl.innerHTML += "<p> Temp: " + data.current.temp + "℉";
       currentCityEl.innerHTML +=
         "<p> Wind: " + data.current.wind_speed + " MPH";
@@ -41,6 +43,8 @@ function getWeatherInfo(latitude, longitude) {
       }
       for (let j = 0; j < 5; j++) {
         document.getElementById(j).innerHTML ="<h3>" + moment().add(j+1, "day").format("(M/D/YYYY)");
+        document.getElementById(j).innerHTML += "<img src=http://openweathermap.org/img/wn/" + data.daily[j].weather[0].icon + "@2x.png>" 
+        document.getElementById(j).innerHTML += "<p> Description: " + data.daily[j].weather[0].description
         document.getElementById(j).innerHTML += "<p> Temp: " + data.daily[j].temp.day + "℉"
         document.getElementById(j).innerHTML += "<p> Humidity: " + data.daily[j].humidity + " %"
         document.getElementById(j).innerHTML += "<p> Wind: " + data.daily[j].wind_speed + " MPH"
